dce65dab67d60088c32a04dcb8ee8e58f0d77d53
Sokly <hengsokly23@gmail.com>
2020-10-05 11:03:04 +0700

    add splash screen

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigator from './app/navigators/app_navigator';
 import * as Sentry from '@sentry/react-native';
 import { SafeAreaView } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 Sentry.init({
   dsn: 'https://5f4fd35d83f1473291df0123fca8ec00@o357910.ingest.sentry.io/5424146',
 });
 
 const App: () => React$Node = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <AppNavigator />
--- android/app/src/main/java/org/instedd/ilabsea/community_scorecard/MainActivity.java
+++ android/app/src/main/java/org/instedd/ilabsea/community_scorecard/MainActivity.java
@@ -1,6 +1,9 @@
 package org.instedd.ilabsea.community_scorecard;
 
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "CscMobile";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);  // here
+    super.onCreate(savedInstanceState);
   }
 }
