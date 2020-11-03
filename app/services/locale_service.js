8ce96d94bc8a223e644502e20e698bbc7358ac33
Kimsanlim <kimsanlim27@gmail.com>
2020-11-03 11:37:18 +0700

    Change locale variable name and use es6 for looping

--- app/services/locale_service.js
+++ app/services/locale_service.js
@@ -3,23 +3,15 @@ import {localeDictionary} from '../constants/locale_constant';
 const getLocaleChoices = (localeCodes) => {
   let localeChoices = [];
   localeCodes.map((code) => {
-    const localeSet = {label: localeDictionary[code], value: code};
-    localeChoices.push(localeSet);
+    const locale = {label: localeDictionary[code], value: code};
+    localeChoices.push(locale);
   });
 
   return localeChoices;
 };
 
 const getLocaleOptions = (locales) => {
-  let localeOptions = [];
-  locales.map((locale) => {
-    const localeSet = {
-      label: locale.name,
-      value: locale.code,
-    };
-    localeOptions.push(localeSet);
-  });
-
+  const localeOptions = locales.map((locale) => ({value: locale.code, label: locale.name}));
   return localeOptions;
 };
 
