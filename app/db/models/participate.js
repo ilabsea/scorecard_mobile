898281cb84ef76bb2c53e231368b77f1bc26b254
Kimsanlim <kimsanlim27@gmail.com>
2020-11-06 15:23:14 +0700

    Apply validation to paticipate form and be able to save input data to local storage

---
+++ app/db/models/participate.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const ParticipateSchema = {
+  name: 'Participate',
+  properties: {
+    allParticipate: 'int',
+    female: 'int',
+    disability: 'int',
+    minority: 'int',
+    poor: 'int',
+    youth: 'int',
+    uuid: 'string',
+  }
+}
+
+export default ParticipateSchema;
