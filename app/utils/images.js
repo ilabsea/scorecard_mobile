297655d7c59321b53f512a13850cd0bc691f3030
Sokly <hengsokly23@gmail.com>
2020-11-16 11:57:14 +0700

    Voting List: update rating icon

---
+++ app/utils/images.js
@@ -0,0 +1,10 @@
+const Images = {
+  // rating icon
+  very_bad: require('../assets/images/rating/very_bad.png'),
+  bad: require('../assets/images/rating/bad.png'),
+  acceptable: require('../assets/images/rating/acceptable.png'),
+  good: require('../assets/images/rating/good.png'),
+  very_good: require('../assets/images/rating/very_good.png'),
+}
+
+export default Images;
