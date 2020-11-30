387e42bdb4c5555592c131247ea393f7f184a5a2
Sokly <hengsokly23@gmail.com>
2020-11-30 11:00:29 +0700

    Home: add background and update button

---
+++ app/components/BigButton.js
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+import { Icon } from 'native-base';
+import PropTypes from 'prop-types';
+
+const BigButton = (props) => {
+  return (
+    <TouchableOpacity
+      onPress={ props.onPress }
+      style={ styles.button }>
+
+      <Icon name={props.icon} style={ styles.icon }/>
+      <Text style={{color: '#fff'}}>{ props.label }</Text>
+    </TouchableOpacity>
+  )
+}
+
+BigButton.propTypes = {
+  label: PropTypes.string,
+  icon: PropTypes.string,
+  onPress: PropTypes.func
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#003b5c',
+    flexDirection: 'row',
+    height: 86,
+    width: 360,
+    alignItems: 'center',
+    borderRadius: 8,
+    marginTop: 20
+  },
+  icon: {
+    color: '#fff',
+    fontSize: 48,
+    marginLeft: 24,
+    marginRight: 40
+  }
+});
+
+
+export default BigButton;
--- app/screens/Home/Home.js
+++ app/screens/Home/Home.js
@@ -1,33 +1,51 @@
 import React, {Component, useContext, useEffect, useState} from 'react';
 import { LocalizationContext } from '../../components/Translations';
 import ActionButton from '../../components/ActionButton';
+import { Button } from 'react-native-paper';
 
 import {
   ScrollView,
   StyleSheet,
+  ImageBackground,
+  View,
+  Image,
+  Text,
 } from "react-native";
+
+import { Icon } from 'native-base';
+import { FontFamily } from '../../assets/stylesheets/theme/font';
+import BigButton from '../../components/BigButton';
 
 class Home extends Component {
   static contextType = LocalizationContext;
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     const {translations} = this.context;
 
     return (
-      <ScrollView contentContainerStyle={styles.container}>
-        <ActionButton
-          onPress={() => this.props.navigation.navigate('NewScorecard')}
-          label={translations['newScorecard']}
-        />
-        <ActionButton
-          onPress={() => this.props.navigation.navigate('ScorecardList')}
-          label={translations['savedScorecard']}
-          customButtonStyle={{marginTop: 26}}
-        />
-      </ScrollView>
+      <ImageBackground source={require('../../assets/images/bg.jpg')} style={styles.image}>
+        <View style={styles.container}>
+          <Image source={require('../../assets/images/care_logo.png')} style={{width: 120, height: 120}}/>
+
+          <View style={{marginBottom: 40, marginTop: 30}}>
+            <Text style={{fontFamily: FontFamily.title, fontSize: 32, color: '#fff'}}>សូមស្វាគមន៍មកកាន់</Text>
+            <Text style={{fontFamily: FontFamily.title, fontSize: 32, color: '#fff'}}>ប័ណ្ណដាក់ពន្ទុឌីជីថល</Text>
+          </View>
+
+          <BigButton
+            onPress={() => this.props.navigation.navigate('NewScorecard')}
+            label={ translations['newScorecard'] }
+            icon={'add-circle-sharp'}
+          />
+
+          <BigButton
+            onPress={() => this.props.navigation.navigate('ScorecardList')}
+            label={ translations['savedScorecard'] }
+            icon={'list-circle'}
+          />
+
+        </View>
+      </ImageBackground>
     );
   }
 }
@@ -35,10 +53,14 @@ class Home extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     padding: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    resizeMode: "cover",
+    justifyContent: "center"
   },
 });
 
