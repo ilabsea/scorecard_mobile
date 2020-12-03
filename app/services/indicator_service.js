30f7b86f22fed5ffdfd012d2f5bcf21575ceddab
Kimsanlim <kimsanlim27@gmail.com>
2020-12-03 15:39:28 +0700

    fix conflict and add tag when saving indciator

--- app/services/indicator_service.js
+++ app/services/indicator_service.js
@@ -2,11 +2,13 @@ import realm from '../db/schema';
 
 const saveIndicator =  (scorecardUUID, indicators, callback) => {
   indicators.map((indicator) => {
+    console.log('save indicator ===== ', indicator)
     const indicatorSet = {
       id: indicator.id,
       name: indicator.name,
       facility_id: indicator.categorizable.id,
       scorecard_uuid: scorecardUUID,
+      tag: indicator.tag_name,
     };
     realm.write(() => { realm.create('Indicator', indicatorSet, 'modified'); });
   });
