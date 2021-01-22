90e67a0ff16b97f06892ea43ba9a73fcbf3a77fb
Sokly <hengsokly23@gmail.com>
2021-01-22 13:28:06 +0700

    participant list: show only existed participant

---
+++ app/components/OutlinedButton.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import { Button, Icon, Text } from 'native-base';
+import Color from '../themes/color';
+
+class OutlinedButton extends Component {
+  render() {
+    return (
+      <Button
+        {...this.props}
+        bordered
+        iconLeft>
+        <Icon name={this.props.icon || 'plus'} type="FontAwesome" style={{color: Color.headerColor}} />
+        <Text style={{color: Color.headerColor}}>{this.props.label}</Text>
+      </Button>
+
+    );
+  }
+}
+
+export default OutlinedButton;
