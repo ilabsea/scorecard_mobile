d46c837701389513cc5a825d561ee1547f02277c
Sokly <hengsokly23@gmail.com>
2021-01-29 11:04:06 +0700

    SWOT: replace median text with icon

--- app/components/ScorecardResult/ScorecardResultTableRow.js
+++ app/components/ScorecardResult/ScorecardResultTableRow.js
@@ -1,102 +1,78 @@
-import React, { useContext } from 'react';
+import React, { Component } from 'react';
 import {
   View,
   StyleSheet,
   Text,
   TouchableOpacity,
-  Alert
 } from 'react-native';
 
 import { Icon } from 'native-base';
 import Color from '../../themes/color';
 
 import { LocalizationContext } from '../Translations';
-import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
-import uuidv4 from '../../utils/uuidv4';
-import ratings from '../../db/jsons/ratings';
-import HTML from "react-native-render-html";
+import { TableWrapper, Cell } from 'react-native-table-component';
 import { getDisplayIndicator } from '../../services/indicator_service';
 
-const ScorecardResultTableRow = (props) => {
-  const { translations } = useContext(LocalizationContext); // 1
-  const { criteria } = props;
-  const indicator = getDisplayIndicator(criteria);
+export default class ScorecardResultTableRow extends Component {
+  static contextType = LocalizationContext;
 
-  const onPress = (fieldName) => {
-    !!props.onPress && props.onPress(fieldName);
+  onPress = (fieldName) => {
+    !!this.props.onPress && this.props.onPress(fieldName);
   }
 
-  const btnAdd = (fieldName) => (
-    <TouchableOpacity onPress={() => onPress(fieldName)}>
-      <View style={styles.btn}>
-        <Text style={styles.btnText}>{ translations.addText }</Text>
-      </View>
-    </TouchableOpacity>
-  );
+  btnAdd = (fieldName) => {
+    const { translations } = this.context;
 
-  const renderEditText = (fieldName) => {
+    return (
+      <TouchableOpacity onPress={() => this.onPress(fieldName)}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>{ translations.addText }</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  renderEditText = (fieldName) => {
     return (
       <View style={{flexDirection: 'row', padding: 6, alignItems: 'center', justifyContent: 'center'}}>
-        <TouchableOpacity onPress={() => onPress(fieldName)} style={{width: 28, height: 28, backgroundColor: Color.headerColor, justifyContent: 'center', alignItems: 'center', borderRadius: 14, marginLeft: 10}}>
+        <TouchableOpacity onPress={() => this.onPress(fieldName)} style={styles.btnEdit}>
           <Icon name={'pen'} type="FontAwesome5" style={{color: '#fff', fontSize: 14}}/>
         </TouchableOpacity>
       </View>
     )
   }
 
-  const tagsStyles = () => {
-    return {
-      div: { maxHeight: 100 },
-      ol: { marginLeft: -20 },
-      ul: { marginLeft: -20 },
-      li: { maxHeight: 100 },
-    };
-  }
-
-  const alterChildren = (node) => {
-    const { children, name } = node;
-    if (name === "ul" || name === "ol" && children && children.length)
-      return children.splice(0, 1);
-    else if (name == "div")
-      return children[0];
-  }
-
-  const alterData = (node) => {
-    let { parent, data } = node;
-
-    if (parent && parent.name === "li")
-      return data.slice(0, 5) + '...';
-
-    return data.slice(0, 15) + '...';
-  }
-
-  const renderCell = (fieldName) => {
-    if (!criteria[fieldName]) {
-      return btnAdd(fieldName);
+  renderCell = (fieldName) => {
+    if (!this.props.criteria[fieldName]) {
+      return this.btnAdd(fieldName);
     }
 
-    return renderEditText(fieldName);
+    return this.renderEditText(fieldName);
   }
 
-  const _renderTextCell = (text, flexNum) => (
+  _renderTextCell = (text, flexNum) => (
     <Cell data={text} textStyle={styles.text} style={{flex: flexNum}}/>
   );
 
-  const editableFields = ['strength',  'weakness', 'desired_change', 'suggested_action'];
-  let median = !!criteria.median ? translations[ratings.filter(x => x.value == criteria.median)[0].label] : '';
-
-  return (
-    <TableWrapper style={styles.row} borderStyle={{borderColor: '#c1c1c1', borderWidth: 1}}>
-      { _renderTextCell(indicator.content || indicator.name, 4) }
-      { _renderTextCell(median, 2) }
-      { editableFields.map((fieldName, index) => (
-        <Cell key={index} data={renderCell(fieldName)} textStyle={styles.text} style={{flex: 3}}/>
-      ))}
-    </TableWrapper>
+  _renderMedian = () => (
+    <Cell data={this.props.criteria.median} style={{flex: 2, alignItems: 'center'}}/>
   )
-}
 
-export default ScorecardResultTableRow;
+  render() {
+    const editableFields = ['strength',  'weakness', 'desired_change', 'suggested_action'];
+    const indicator = getDisplayIndicator(this.props.criteria);
+
+    return (
+      <TableWrapper style={styles.row} borderStyle={{borderColor: '#c1c1c1', borderWidth: 1}}>
+        { this._renderTextCell(indicator.content || indicator.name, 4) }
+        { this._renderMedian() }
+        { editableFields.map((fieldName, index) => (
+          <Cell key={index} data={this.renderCell(fieldName)} textStyle={styles.text} style={{flex: 3}}/>
+        ))}
+      </TableWrapper>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -126,5 +102,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#000',
     fontSize: 14,
+  },
+  btnEdit: {
+    width: 28,
+    height: 28,
+    backgroundColor: Color.headerColor,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 14,
+    marginLeft: 10
   }
 });
