823a67784bb58abd08463d5aa32156bf0ee2492f
Sokly <hengsokly23@gmail.com>
2021-02-23 11:57:34 +0700

    Scorecard queue: move valid day to environment variable

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,6 +1,5 @@
 export const environment = {
-  apiUrl: 'http://192.168.0.107:3000/api/v1',
   domain: 'http://192.168.0.107:3000',
-  access_token: 'token',
   type: 'development',
+  validDay: 30,
 };
--- app/utils/queue.js
+++ app/utils/queue.js
@@ -1,5 +1,6 @@
 import realm from '../db/schema';
 import ScorecardService from '../services/scorecardService';
+import environment from '../config/environment';
 
 export default class Queue {
   static async initWorker() {
@@ -12,7 +13,7 @@ export default class Queue {
       let differenceInTime = today.getTime() - uploadedDate.getTime();
       let differenceInDays = differenceInTime / (1000 * 3600 * 24);
 
-      if (differenceInDays >= 30) {
+      if (differenceInDays > environment.validDay) {
         const scorecardService = new ScorecardService();
         scorecardService.delete(scorecards[i].uuid);
       }
