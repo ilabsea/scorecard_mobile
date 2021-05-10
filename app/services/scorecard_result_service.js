d111caa1f6ea8519ad9354ebe2d5d51ec2544feb
Kimsanlim <kimsanlim27@gmail.com>
2021-05-10 10:12:06 +0700

    Enhable finish button when user input suggested action in all indicator

--- app/services/scorecard_result_service.js
+++ app/services/scorecard_result_service.js
@@ -1,25 +1,20 @@
 const scorecardResultService = (() => {
   return {
-    hasSwotData,
     isAllowToFinish,
   };
 
-  function hasSwotData(criterias) {
-    for (let i=0; i<criterias.length; i++) {
-      const criteria = criterias[i];
-
-      if (criteria.strength || criteria.weakness || criteria.suggested_action)
-        return true;
-    }
-
-    return false;
-  }
-
-  function isAllowToFinish(scorecard, criteria) {
+  function isAllowToFinish(scorecard, criterias) {
     if (scorecard.finished)
       return false;
 
-    return hasSwotData(criteria);
+    for (let i=0; i<criterias.length; i++) {
+      const criteria = criterias[i];
+
+      if (!criteria.suggested_action)
+        return false;
+    }
+
+    return true;
   }
 })();
 
