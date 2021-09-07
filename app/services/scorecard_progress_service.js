a7ef2d4a769a6fdea380aab874e287da1f326d12
Kimsanlim <kimsanlim27@gmail.com>
2021-09-07 16:37:07 +0700

    refactor method that generate info message

--- app/services/scorecard_progress_service.js
+++ app/services/scorecard_progress_service.js
@@ -20,25 +20,17 @@ const scorecardProgressService = (() => {
   }
 
   function getProgressMessage(criterias, scorecard) {
-    let message = '';
     if (scorecard.finished)
       return '';
 
-    if (scorecard.status < VOTING)
-      message = 'pleaseCompleteAllTheSteps';
-    else if (!_isAllCriteriaVoted(criterias, scorecard.uuid))
-      message = 'allCriteriaMustBeVoted';
-    else if (!isAllowToFinish(scorecard))
-      message = 'allCriteriaMustHaveSuggestedAction';
-
-    return message;
-  }
-
-  // private method
-  function _isAllCriteriaVoted(criterias, scorecardUuid) {
-    let votingCriterias = criterias.length > 0 ? criterias : votingCriteriaService.getAll(scorecardUuid);
-
-    return votingCriterias.filter(criteria => !criteria.median).length > 0 ? false : true;
+    let votingCriterias = criterias.length > 0 ? criterias : votingCriteriaService.getAll(scorecard.uuid);
+    const messages = [
+      { label: scorecard.status < VOTING ? 'pleaseCompleteAllTheSteps' : null },
+      { label: votingCriterias.filter(criteria => !criteria.median).length > 0 ? 'allCriteriaMustBeVoted' : null },
+      { label: votingCriterias.filter(criteria => !criteria.suggested_action).length > 0 ? 'allCriteriaMustHaveSuggestedAction' : null },
+    ]
+    const infoMessages = messages.filter(message => message.label);
+    return infoMessages.length > 0 ? infoMessages[0].label : '';
   }
 })();
 
