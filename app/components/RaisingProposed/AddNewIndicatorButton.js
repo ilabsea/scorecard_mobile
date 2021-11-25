a0eab7402fe15703669c4e5ad51e16c76ba733f3
Kimsanlim <kimsanlim27@gmail.com>
2021-11-25 17:28:30 +0700

    Show tourtip on the add new indicator button

--- app/components/RaisingProposed/AddNewIndicatorButton.js
+++ app/components/RaisingProposed/AddNewIndicatorButton.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { copilot, walkthroughable, CopilotStep } from "react-native-copilot";
+import AsyncStorage from '@react-native-community/async-storage';
 
 import Color from '../../themes/color';
 import { LocalizationContext } from '../Translations';
+import TourTipButton from '../TourTipButton';
 import { normalLabelSize } from '../../utils/responsive_util';
 
+const WalkableView = walkthroughable(View);
 class AddNewIndicatorButton extends Component {
   static contextType = LocalizationContext;
 
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasTourtip: false,
+    }
+
+    this.isComponentUnmount = false;
+  }
+
+  async componentDidMount() {
+    const tourtipShown = await AsyncStorage.getItem('ADD_NEW_INDICATOR_TOURTIP');
+
+    if (!this.isComponentUnmount && !tourtipShown) {
+      this.setState({ hasTourtip: true });
+
+      setTimeout(() => {
+          this.props.start();
+      }, 1500)
+    }
+
+    this.props.copilotEvents.on("stop", () => {
+      AsyncStorage.setItem('ADD_NEW_INDICATOR_TOURTIP', 'true');
+      this.setState({ hasTourtip: false });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.copilotEvents.off("stop");
+    this.isComponentUnmount = true;
+  }
+
+  renderButton() {
     return (
       <TouchableOpacity onPress={() => this.props.showAddNewIndicatorForm()}
-        style={styles.container}
+        style={[styles.container, !this.state.hasTourtip ? styles.floatPosition : {}]}
       >
         <Icon name='add' size={30} color={Color.whiteColor} />
         { this.props.scrollDirection == 'up' &&
@@ -21,14 +57,33 @@ class AddNewIndicatorButton extends Component {
       </TouchableOpacity>
     )
   }
+
+  renderButtonWithToolTip() {
+    const walkableWidth = this.context.appLanguage == 'km' ? 250 : 188;
+
+    return (
+      <CopilotStep
+        text={this.context.translations.clickThisButtonToAddNewCriteria}
+        order={1}
+        name="addNewIndicator"
+      >
+        <WalkableView style={{ position: 'absolute', bottom: 8, right: 0, width: walkableWidth, height: 55 }}>
+          { this.renderButton() }
+        </WalkableView>
+      </CopilotStep>
+    )
+  }
+
+  render() {
+    if (this.state.hasTourtip)
+      return this.renderButtonWithToolTip();
+
+    return this.renderButton();
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    bottom: 8,
-    right: 0,
-    zIndex: 10,
     backgroundColor: Color.clickableColor,
     flexDirection: 'row',
     borderRadius: 30,
@@ -36,7 +91,22 @@ const styles = StyleSheet.create({
     elevation: 4,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  floatPosition: {
+    position: 'absolute',
+    bottom: 8,
+    right: 0,
+    zIndex: 10,
   }
 });
 
-export default AddNewIndicatorButton;
+export default copilot({
+  overlay: 'svg',
+  animated: true,
+  verticalOffset: 24,
+  backdropColor: "rgba(31, 31, 31, 0.7)",
+  labels: {
+    finish: <TourTipButton label='infoCloseLabel' />
+  },
+  stepNumberComponent: () => (<View/>)
+})(AddNewIndicatorButton);
