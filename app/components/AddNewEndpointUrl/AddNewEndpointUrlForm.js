eb1d23fa454f5db62e7a1c51b4e508cdb31692ac
Kimsanlim <kimsanlim27@gmail.com>
2022-05-25 18:00:16 +0700

    Hide the edit button when the endpoint is selected

--- app/components/AddNewEndpointUrl/AddNewEndpointUrlForm.js
+++ app/components/AddNewEndpointUrl/AddNewEndpointUrlForm.js
@@ -21,6 +21,20 @@ class AddNewEndpointUrlForm extends React.Component {
     }
   }
 
+  async componentDidMount() {
+    const selectedEndpoint = await endpointFormService.getEndpointForEdit();
+
+    if (!!selectedEndpoint) {
+      this.setState({
+        selectedEndpoint: selectedEndpoint,
+        endpointLabel: selectedEndpoint.label,
+        endpointValue: selectedEndpoint.value.toString(),
+        endpointUuid: selectedEndpoint.uuid,
+        isAllowToDeleteOrEdit: await endpointFormService.isAllowToDeleteOrEdit(selectedEndpoint),
+      });
+    }
+  }
+
   onChangeText = (fieldName, value) => {
     let state = {};
     state[fieldName] = value;
