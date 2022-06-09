8d97c16cfe5b60870685ad2873db27e3206e1fbd
Kimsanlim <kimsanlim27@gmail.com>
2022-06-09 14:07:23 +0700

    refactor setting header and add new endpoint form header

--- app/components/AddNewEndpointUrl/AddNewEndpointUrlHeader.js
+++ app/components/AddNewEndpointUrl/AddNewEndpointUrlHeader.js
@@ -1,49 +1,24 @@
 import React from 'react';
 
 import { LocalizationContext } from '../Translations';
-import HeaderWithConfirmModal from '../Share/HeaderWithConfirmModal';
+import HeaderWithDiscardAlert from '../Share/HeaderWithDiscardAlert';
 
-import { navigateBack } from '../../utils/navigation_util';
-import endpointFormService from '../../services/endpoint_form_service';
 import endpointFormHelper from '../../helpers/endpoint_form_helper';
 
 class AddNewEndpointUrlHeader extends React.Component {
   static contextType = LocalizationContext;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      visibleConfirmModal: false,
-    }
-  }
-
-  onBackPress() {
+  hasDiscardAlert() {
     const { endpointLabel, endpointValue } = this.props.bodyRef.current?.inputFormRef.current?.state;
-
-    if (!!endpointLabel || !!endpointValue)
-      this.setState({ visibleConfirmModal: true })
-    else
-      this.goBack();
-  }
-
-  goBack() {
-    this.setState({ visibleConfirmModal: false }, () => {
-      endpointFormHelper.clearHasNewEndpointAdded();
-      navigateBack();
-    });
+    return !!endpointLabel || !!endpointValue
   }
 
   render() {
-    const { translations } = this.context;
-
-    return <HeaderWithConfirmModal
-              title={translations.newServerURL}
-              modalDescription={translations.doYouWantToDiscardTheseChanges}
-              visibleConfirmModal={this.state.visibleConfirmModal}
-              onBackPress={() => this.onBackPress()}
-              goBack={() => this.goBack()}
-              onDismiss={() => this.setState({ visibleConfirmModal: false })}
-           />
+    return <HeaderWithDiscardAlert
+              title={this.context.translations.newServerURL}
+              onGoBack={() => endpointFormHelper.clearHasNewEndpointAdded()}
+              hasDiscardAlert={() => this.hasDiscardAlert()}
+            />
   }
 }
 
--- app/components/Setting/SettingHeader.js
+++ app/components/Setting/SettingHeader.js
@@ -1,44 +1,29 @@
 import React from 'react';
 
 import { LocalizationContext } from '../Translations';
-import HeaderWithConfirmModal from '../Share/HeaderWithConfirmModal';
+import HeaderWithDiscardAlert from '../Share/HeaderWithDiscardAlert';
 
-import { navigateBack } from '../../utils/navigation_util';
 import settingHelper from '../../helpers/setting_helper';
 import endpointFormHelper from '../../helpers/endpoint_form_helper';
 
 class SettingHeader extends React.Component {
   static contextType = LocalizationContext;
-  state = {
-    visibleConfirmModal: false,
+
+  async hasDiscardAlert() {
+    return await settingHelper.hasDiscardAlert();
   }
 
-  async onBackPress() {
-    if (await settingHelper.hasDiscardAlert())
-      this.setState({ visibleConfirmModal: true })
-    else
-      this.goBack();
-  }
-
-  goBack() {
-    this.setState({ visibleConfirmModal: false }, () => {
-      settingHelper.clearTempSettingData();
-      endpointFormHelper.clearHasNewEndpointAdded();
-      navigateBack();
-    });
+  onGoBack() {
+    settingHelper.clearTempSettingData();
+    endpointFormHelper.clearHasNewEndpointAdded();
   }
 
   render() {
-    const { translations } = this.context;
-
-    return <HeaderWithConfirmModal
-              title={translations.setting}
-              modalDescription={translations.doYouWantToDiscardTheseChanges}
-              visibleConfirmModal={this.state.visibleConfirmModal}
-              onBackPress={() => this.onBackPress()}
-              goBack={() => this.goBack()}
-              onDismiss={() => this.setState({ visibleConfirmModal: false })}
-           />
+    return <HeaderWithDiscardAlert
+              title={this.context.translations.setting}
+              onGoBack={() => this.onGoBack()}
+              hasDiscardAlert={() => this.hasDiscardAlert()}
+            />
   }
 }
 
