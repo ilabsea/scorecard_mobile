372110128b4dc104f53d6b4f6f8424c6ba73d9c5
Kimsanlim <kimsanlim27@gmail.com>
2022-06-22 10:52:37 +0700

    Update the contact screen UI

--- app/components/Contact/ContactListItem.js
+++ app/components/Contact/ContactListItem.js
@@ -1,66 +1,64 @@
-import React, {Component} from 'react';
-
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 import { LocalizationContext } from '../../components/Translations';
 import { Icon } from 'native-base';
 import Color from '../../themes/color';
-import uuidV4 from '../../utils/uuidv4';
-import styles from '../../themes/scorecardListItemStyle';
-import { cardBorderRadius } from '../../constants/border_radius_constant';
+import listItemStyles from '../../themes/scorecardListItemStyle';
 
+import { bodyFontSize, mediumIconSize } from '../../utils/font_size_util';
 import { getDeviceStyle } from '../../utils/responsive_util';
-import ContactListItemTabletStyles from '../../styles/tablet/ContactListItemComponentStyle';
-import ContactListItemMobileStyles from '../../styles/mobile/ContactListItemComponentStyle';
 
-const responsiveStyles = getDeviceStyle(ContactListItemTabletStyles, ContactListItemMobileStyles);
-
-export default class ContactListItem extends Component {
+export default class ContactListItem extends React.Component {
   static contextType = LocalizationContext;
 
-  renderStatusIcon(item, iconName) {
+  renderContactInfo(item) {
     return (
-      <View style={[styles.statusIconWrapper, { borderTopLeftRadius: cardBorderRadius, borderBottomLeftRadius: cardBorderRadius }]}>
-        <Icon name={iconName} style={responsiveStyles.statusIcon} />
-      </View>
+      <React.Fragment>
+        <View style={{ flex: 1 }}>
+          <Text style={{fontSize: bodyFontSize()}}>{ item.title }</Text>
+          <Text style={{fontSize: bodyFontSize()}}>{ this.props.contact.value }</Text>
+        </View>
+        <View style={styles.rightItemContainer}>
+          <Text style={{color: Color.clickableColor, fontSize: bodyFontSize()}}>{this.context.translations.contact}</Text>
+          <Icon name='chevron-forward-outline' style={{color: Color.clickableColor, fontSize: mediumIconSize()}} />
+        </View>
+      </React.Fragment>
     )
   }
 
   render() {
     const { translations } = this.context;
-    const { contact } = this.props;
-    const iconName = contact.contact_type == 'email' ? 'at' : 'call';
-    const title = contact.contact_type == 'email' ? translations.contact_email : translations.contact_number;
+    const item = this.props.contact.contact_type == 'email' ?
+                 { title: translations.contact_email, icon: 'at', icon_color: 'red' }
+                 : { title: translations.contact_number, icon: 'call', icon_color: '#1221ca' }
 
     return (
       <TouchableOpacity
-        key={uuidV4()}
         onPress={ () => !!this.props.onPress && this.props.onPress(this.props.contact) }
-        style={[styles.listItem, styles.card]}>
-
-        { this.renderStatusIcon(contact, iconName) }
-
-        <View style={styles.contentWrapper}>
-          <Text style={styles.title} numberOfLines={1}>{title}</Text>
-
-          <View style={styles.subTextWrapper}>
-            <Icon name={iconName} style={styles.subTextIcon} />
-            <Text style={[styles.subText, responsiveStyles.subText]}>{contact.value}</Text>
-          </View>
-
-          <View style={{flex: 1}}></View>
-
-          <View style={styles.viewDetail}>
-            <Text style={[{color: Color.headerColor}, responsiveStyles.viewDetailLabel]}>{translations.contact}</Text>
-            <Icon name='chevron-forward-outline' style={[{color: Color.headerColor}, responsiveStyles.viewDetailIcon]} />
-          </View>
-        </View>
+        style={[listItemStyles.listItem, listItemStyles.card, styles.container]}
+      >
+        <Icon name={item.icon} style={[styles.icon, { color: item.icon_color}]} />
+        { this.renderContactInfo(item) }
       </TouchableOpacity>
     )
   }
 }
+
+const spacingSize = 16;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacingSize,
+    alignItems: 'center',
+    marginBottom: spacingSize
+  },
+  icon: {
+    fontSize: getDeviceStyle(45, 35),
+    marginRight: spacingSize
+  },
+  rightItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+});
