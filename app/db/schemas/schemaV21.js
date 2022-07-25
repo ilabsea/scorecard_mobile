228cec56d4b28ebb4825aee54d60feecb804fb9d
Kimsanlim <kimsanlim27@gmail.com>
2022-07-25 15:56:32 +0700

    Create migration to update the participant counted field

--- app/db/schemas/schemaV21.js
+++ app/db/schemas/schemaV21.js
@@ -1,22 +1,23 @@
-import ProgramLanguage from '../migrations/v3/programLanguage';
-import Scorecard from '../migrations/v21/scorecard';
-import ProposedIndicator from '../migrations/v12/proposedIndicator';
-import Indicator from '../migrations/v19/indicator';
-import LanguageIndicator from '../migrations/v19/languageIndicator';
-import EndpointUrl from '../migrations/v20/endpointUrl';
-import Rating from '../migrations/v14/rating';
-import VotingIndicator from '../migrations/v14/votingIndicator';
-import Facilitator from '../migrations/v16/facilitator';
+import ProgramLanguage from '../../migrations/v3/programLanguage';
+import Scorecard from '../../migrations/v21/scorecard';
+import ProposedIndicator from '../../migrations/v12/proposedIndicator';
+import Indicator from '../../migrations/v19/indicator';
+import LanguageIndicator from '../../migrations/v19/languageIndicator';
+import EndpointUrl from '../../migrations/v20/endpointUrl';
+import Rating from '../../migrations/v14/rating';
+import VotingIndicator from '../../migrations/v14/votingIndicator';
+import Facilitator from '../../migrations/v16/facilitator';
+import Participant from '../../migrations/v21/participant';
 
-import schemaHelper from '../../helpers/schema_helper';
-import { schemaNames } from '../../constants/schema_constant';
-import { DOWNLOADED } from '../../constants/scorecard_constant';
+import schemaHelper from '../../../helpers/schema_helper';
+import { schemaNames } from '../../../constants/schema_constant';
 
 const changedSchemas = [
   { label: schemaNames[0], data: Scorecard },
   { label: schemaNames[2], data: Indicator },
   { label: schemaNames[4], data: LanguageIndicator },
   { label: schemaNames[5], data: Facilitator },
+  { label: schemaNames[6], data: Participant },
   { label: schemaNames[10], data: Rating },
   { label: schemaNames[13], data: ProgramLanguage },
   { label: schemaNames[17], data: ProposedIndicator },
@@ -24,7 +25,7 @@ const changedSchemas = [
   { label: schemaNames[20], data: EndpointUrl }
 ];
 
-const schemaV20 = {
+const schemaV21 = {
   schema: schemaHelper.getSchemas(changedSchemas),
   schemaVersion: 21,
   migration: (oldRealm, newRealm) => {
@@ -37,8 +38,15 @@ const schemaV20 = {
         const isUploaded = (!oldScorecards[i].milestone || oldScorecards[i].milestone == DOWNLOADED) ? false : true;
         newScorecards[i].running_status_uploaded = !oldScorecards[i].running_status_uploaded ? isUploaded : oldScorecards[i].running_status_uploaded;
       }
+
+      const oldParticipants = oldRealm.objects('Participant');
+      const newParticipants = newRealm.objects('Participant');
+
+      for (let i = 0; i < oldParticipants.length; i ++) {
+        newParticipants[i].counted = !oldParticipants[i].counted ? true : oldParticipants[i].counted;
+      }
     }
   }
 }
 
-export default schemaV20;
+export default schemaV21;
