73f030d739aa131a83328c5c70d2580e54605c65
Kimsanlim <kimsanlim27@gmail.com>
2022-08-04 10:08:24 +0700

    Move the participant attributes select boxes and uncountable select box to sub-component

--- app/components/AddNewParticipant/ParticipantForm.js
+++ app/components/AddNewParticipant/ParticipantForm.js
@@ -1,18 +1,15 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableWithoutFeedback, Keyboard} from 'react-native';
-import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import {View, TouchableWithoutFeedback, Keyboard} from 'react-native';
 
 import {LocalizationContext} from '../Translations';
 import NumericInput from '../Share/NumericInput';
+import AttributeSelectBoxes from './AttributeSelectBoxes';
+import UncountableSelectBox from './UncountableSelectBox';
+import GendersCheckBox from './GendersCheckBox';
+
 import { getIntegerOf } from '../../utils/math';
-import uuidv4 from '../../utils/uuidv4'
-import { getDeviceStyle } from '../../utils/responsive_util';
-import { bodyFontSize } from '../../utils/font_size_util';
 import { MALE } from '../../constants/participant_constant';
 import participantHelper from '../../helpers/participant_helper';
-
-import GendersCheckBox from './GendersCheckBox';
-import OptionsSelectBox from './OptionsSelectBox';
 
 class ParticipantForm extends Component {
   static contextType = LocalizationContext;
@@ -66,69 +63,25 @@ class ParticipantForm extends Component {
   };
 
   _renderParticipantAttributes = () => {
-    const { translations } = this.context;
-    const attributes = {
-      firstRow: [
-        { iconName: 'wheelchair', fieldName: 'isDisability', isSelected: this.state.isDisability, title: translations.disability },
-        { iconName: 'users', fieldName: 'isMinority', isSelected: this.state.isMinority, title: translations.minority },
-        { iconName: 'id-card-o', fieldName: 'isPoor', isSelected: this.state.isPoor, title: translations.poor },
-      ]
-    }
-
-    let doms = [];
-    for (let key in attributes) {
-      doms.push(
-        <View key={uuidv4()} style={{flexDirection: 'row', marginTop: 10, justifyContent: 'space-between'}}>
-          {
-            attributes[key].map((attribute) => {
-              if (attribute == null)
-                return (<View key={uuidv4()} style={{flex: 1}} />)
-
-              return (
-                <View key={uuidv4()} style={{ marginBottom: 10, flex: 1, alignItems: 'center' }}>
-                  <OptionsSelectBox
-                    title={attribute.title}
-                    iconName={attribute.iconName}
-                    fieldName={attribute.fieldName}
-                    onChangeValue={this.onChangeValue}
-                    isSelected={attribute.isSelected}
-                    renderSmallSize={this.props.renderSmallSize}
-                    disabled={this.state.uncountable}
-                  />
-                </View>
-              )
-            })
-          }
-        </View>
-      )
-    }
-
-    return doms;
+    return <AttributeSelectBoxes
+              isDisability={this.state.isDisability}
+              isMinority={this.state.isMinority}
+              isPoor={this.state.isPoor}
+              renderSmallSize={this.props.renderSmallSize}
+              disabled={this.state.uncountable}
+              onChange={this.onChangeValue}
+           />
   }
 
   _renderUncountableOption = () => {
     if (participantHelper.isUncountableOptionVisible(this.props.scorecardUuid) || this.props.isUpdate)
       return;
 
-    return (
-      <View>
-        <Text style={{marginTop: 5, fontSize: bodyFontSize()}}>
-          { this.context.translations.other } ({ this.context.translations.anonymous })
-        </Text>
-
-        <View style={{ marginTop: 8, paddingLeft: wp(getDeviceStyle('8.3%', '4.5%')) }}>
-          <OptionsSelectBox
-            title={ this.context.translations.anonymous }
-            iconName='eye-off'
-            iconType='material'
-            fieldName='uncountable'
-            onChangeValue={this.onUncountableChange}
-            isSelected={this.state.uncountable}
-            renderSmallSize={this.props.renderSmallSize}
-          />
-        </View>
-      </View>
-    )
+    return <UncountableSelectBox
+              value={this.state.uncountable}
+              onChange={this.onUncountableChange}
+              renderSmallSize={this.props.renderSmallSize}
+           />
   }
 
   render() {
@@ -158,9 +111,6 @@ class ParticipantForm extends Component {
             disabled={this.state.uncountable}
           />
 
-          <Text style={{marginTop: 15, fontSize: bodyFontSize()}}>
-            { translations.attributes }
-          </Text>
           { this._renderParticipantAttributes() }
           { this._renderUncountableOption() }
         </View>
---
+++ app/components/AddNewParticipant/UncountableSelectBox.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+
+import {LocalizationContext} from '../Translations';
+import OptionsSelectBox from './OptionsSelectBox';
+import { getDeviceStyle } from '../../utils/responsive_util';
+import { bodyFontSize } from '../../utils/font_size_util';
+
+class UncountableSelectBox extends React.Component {
+  static contextType = LocalizationContext;
+
+  render() {
+    return (
+      <View>
+        <Text style={{marginTop: 5, fontSize: bodyFontSize()}}>
+          { this.context.translations.other } ({ this.context.translations.anonymous })
+        </Text>
+
+        <View style={{ marginTop: 8, paddingLeft: wp(getDeviceStyle('8.3%', '4.5%')) }}>
+          <OptionsSelectBox
+            title={ this.context.translations.anonymous }
+            iconName='eye-off'
+            iconType='material'
+            fieldName='uncountable'
+            onChangeValue={this.props.onChange}
+            isSelected={this.props.value}
+            renderSmallSize={this.props.renderSmallSize}
+          />
+        </View>
+      </View>
+    )
+  }
+}
+
+export default UncountableSelectBox;
