717c65b9cd41ab34f7f23a17c7ec47523cfb2dc4
Kimsanlim <kimsanlim27@gmail.com>
2023-02-02 17:02:59 +0700

    Add react-native-audio-player-button lib and apply to indicator card item in the proposed indicator screen

--- app/components/ProposedIndicator/IndicatorAudioButton.js
+++ app/components/ProposedIndicator/IndicatorAudioButton.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import { View } from 'react-native';
+
 import {getLanguageIndicator} from '../../services/language_indicator_service';
 
-import PlaySound from '../Share/PlaySound';
+// import PlaySound from '../Share/PlaySound';
+import CustomAudioPlayerButton from '../Share/CustomAudioPlayerButton';
 
 class IndicatorAudioButton extends Component {
   state = {
@@ -20,9 +22,17 @@ class IndicatorAudioButton extends Component {
 
   render() {
     return (
-      <View style={{justifyContent: 'center'}}>
-        <PlaySound filePath={this.state.audioFile}
+      <View style={{justifyContent: 'center', borderWidth: 0}}>
+        {/* <PlaySound filePath={this.state.audioFile}
           onPress={() => this.props.updatePlayingIndicatorId(this.props.indicator.uuid)}
+        /> */}
+
+        <CustomAudioPlayerButton
+          audio={this.state.audioFile}
+          itemUuid={this.props.indicator.uuid}
+          playingUuid={this.props.playingIndicatorId}
+          updatePlayingUuid={(uuid) => this.props.updatePlayingIndicatorId(uuid)}
+          buttonStyle={{borderRadius: 0, alignItems: 'flex-end'}}
         />
       </View>
     );
