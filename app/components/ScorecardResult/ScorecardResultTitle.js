956d61fd3836130556461b24782ac19121e89c5e
Kimsanlim <kimsanlim27@gmail.com>
2023-05-09 09:39:43 +0700

    Apply collapsible nav header to scorecard result screen

--- app/components/ScorecardResult/ScorecardResultTitle.js
+++ app/components/ScorecardResult/ScorecardResultTitle.js
@@ -4,8 +4,9 @@ import { View, Text } from 'react-native';
 import { LocalizationContext } from '../../components/Translations';
 import ViewImageButton from './ViewImageButton';
 import { FontFamily } from '../../assets/stylesheets/theme/font';
-import { getDeviceStyle } from '../../utils/responsive_util';
+import { getDeviceStyle, containerPadding } from '../../utils/responsive_util';
 import { titleFontSize } from '../../utils/font_size_util';
+import Color from '../../themes/color';
 
 class ScorecardResultTitle extends Component {
   static contextType = LocalizationContext;
@@ -14,7 +15,7 @@ class ScorecardResultTitle extends Component {
     const { translations } = this.context;
 
     return (
-      <View style={{ flexDirection: 'row', marginBottom: getDeviceStyle(20, 5) }}>
+      <View style={{ flexDirection: 'row', padding: containerPadding, paddingTop: 0, backgroundColor: Color.defaultBgColor}}>
         <View style={{flex: 1, justifyContent: 'center'}}>
           <Text style={{ fontSize: getDeviceStyle(24, titleFontSize()), fontFamily: FontFamily.title }}>
             { translations.scorecardResult }
