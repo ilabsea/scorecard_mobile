2c11bbf314ac0e9948939149c4180d2ec963b48a
Kimsanlim <kimsanlim27@gmail.com>
2025-12-29 10:21:57 +0700

    Update the staging and production endpoints

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,11 +1,11 @@
 export const environment = {
-  domain: 'https://scorecard-stg.dojoconnect.co',
+  domain: 'https://digitalscorecard.org',
   type: 'development',
   defaultLanguage: 'km',
   removeScorecardDay: 90,
   numberOfFacilitators: 4,
   implementedDayRange: 14,     // days
-  defaultEndpoint: 'https://scorecard-stg.dojoconnect.co',
+  defaultEndpoint: 'https://digitalscorecard.org',
   maxSignInAttempt: 10,
   maxJoinScorecardAttempt: 10,
   resetLockDuration: 900000,       // milliseconds (15 mins)
