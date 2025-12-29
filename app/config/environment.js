f69d570cff796b31ea03a544574e6b2ce6394500
Kimsanlim <kimsanlim27@gmail.com>
2025-12-29 11:43:44 +0700

    Change the default endpoint to the new staging endpoint

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,11 +1,11 @@
 export const environment = {
-  domain: 'https://digitalscorecard.org',
+  domain: 'https://staging.digitalscorecard.org',
   type: 'development',
   defaultLanguage: 'km',
   removeScorecardDay: 90,
   numberOfFacilitators: 4,
   implementedDayRange: 14,     // days
-  defaultEndpoint: 'https://digitalscorecard.org',
+  defaultEndpoint: 'https://staging.digitalscorecard.org',
   maxSignInAttempt: 10,
   maxJoinScorecardAttempt: 10,
   resetLockDuration: 900000,       // milliseconds (15 mins)
