92b3245fe7cdfcb62416aad21a37e1ae1df57839
Kimsanlim <kimsanlim27@gmail.com>
2026-01-10 10:10:56 +0700

    Add buttons for open voting and download the QR code in the VotingQr screen

---
+++ app/components/VotingQr/VotingQrCode.js
@@ -0,0 +1,82 @@
+import React, {useContext} from 'react';
+import { View, Image, Dimensions, Linking, StyleSheet } from 'react-native';
+import { Text } from 'react-native-paper';
+import Share from 'react-native-share';
+
+import Color from '../../themes/color';
+import { LocalizationContext } from '../Translations';
+import OutlinedButton from '../OutlinedButton';
+import { getDeviceStyle } from '../../utils/responsive_util';
+import { ERROR_SOMETHING_WENT_WRONG } from '../../constants/error_constant';
+
+import { FontFamily } from '../../assets/stylesheets/theme/font';
+// import { xxlLabelSize, xlLabelSize } from '../../constants/mobile_font_size_constant';
+// import Color from '../../themes/color';
+import { bodyFontSize } from '../../utils/font_size_util';
+
+const contentFontSize = bodyFontSize();
+
+const VotingQrCode = (props) => {
+  const { translations } = useContext(LocalizationContext);
+  const screenWidth = Dimensions.get('screen').width;
+
+  const shareLink = () => {
+    Share.open({ url: props.votingObj.url, failOnCancel: false })
+      .catch((error) => {
+        // Todo: update the error message when failed to share the VOTING LINK
+        props.showErrorModal(ERROR_SOMETHING_WENT_WRONG);
+      })
+  }
+
+  return (
+    <React.Fragment>
+
+      <Text style={[styles.label, { marginTop: 16, textAlign: 'center' }]}>
+        { translations.scanQrCodeToVote }
+      </Text>
+
+      <Image source={{ uri: `file://${props.votingObj.qr_code}` }}
+        style={{width: screenWidth - 100, height: screenWidth - 100, alignSelf: 'center', marginTop: 22}}
+      />
+
+      <Text style={[styles.label, { marginTop: 16, textAlign: 'center' }]}>
+        { translations.orVoteViaTheLink }
+      </Text>
+      <View style={{flexDirection: 'row', justifyContent: 'space-around', marginTop: 16, paddingHorizontal: 16}}>
+        <View style={{flex: 1}}>
+          <OutlinedButton
+            icon="add-outline"
+            label={translations.voting}
+            onPress={() => {
+              if (!!props.votingObj.url)
+                Linking.openURL(props.votingObj.url);
+            }}
+            labelStyle={{textAlign: 'center', fontSize: getDeviceStyle(18, 14), marginTop: getDeviceStyle(6, 4), color: 'white'}}
+            iconStyle={{color: 'white'}}
+          />
+        </View>
+
+        <View style={{width: 16}}/>
+
+        <View style={{flex: 1}}>
+          <OutlinedButton
+            icon="share-outline"
+            label={translations.shareLink}
+            onPress={() => shareLink() }
+            buttonStyle={{ backgroundColor: 'transparent', borderWidth: 2, borderColor: Color.primaryButtonColor }}
+            labelStyle={{textAlign: 'center', fontSize: getDeviceStyle(18, 14), marginTop: getDeviceStyle(6, 4)}}
+          />
+        </View>
+      </View>
+    </React.Fragment>
+  )
+}
+
+const styles = StyleSheet.create({
+  label: {
+    fontSize: contentFontSize,
+    fontFamily: FontFamily.body
+  }
+});
+
+export default VotingQrCode;
--- app/screens/VotingQr/VotingQr.js
+++ app/screens/VotingQr/VotingQr.js
@@ -2,13 +2,16 @@ import React, {useContext, useEffect, useState} from 'react';
 import { Animated, View, ScrollView, Image, Dimensions, Linking, TouchableOpacity } from 'react-native';
 import { Text } from 'react-native-paper';
 import { connect } from 'react-redux';
-import Share from 'react-native-share';
+import {heightPercentageToDP as hp, widthPercentageToDP as wp} from 'react-native-responsive-screen';
+// import Share from 'react-native-share';
 
 import Color from '../../themes/color';
 import { LocalizationContext } from '../../components/Translations';
 import CollapsibleNavHeader from '../../components/Share/CollapsibleNavHeader';
 import ErrorAlertMessage from '../../components/Share/ErrorAlertMessage';
 import BottomButton from '../../components/BottomButton';
+import OutlinedButton from '../../components/OutlinedButton';
+import EmptyListAction from '../../components/Share/EmptyListAction';
 import { screenPaddingBottom } from '../../utils/component_util';
 import {
   bottomButtonContainerPadding,
@@ -21,12 +24,13 @@ import Scorecard from '../../models/Scorecard';
 import scorecardProgressService from '../../services/scorecard_progress_service';
 import ScorecardApi from '../../api/ScorecardApi';
 import onlineScorecardSubmissionService from '../../services/online_scorecard_submission_service';
-import OutlinedButton from '../../components/OutlinedButton';
-import { ERROR_DOWNLOAD_VOTING_QR, ERROR_SOMETHING_WENT_WRONG } from '../../constants/error_constant';
+import VotingQrCode from '../../components/VotingQr/VotingQrCode';
+import { ERROR_DOWNLOAD_VOTING_QR } from '../../constants/error_constant';
 
 const VotingQr = (props) => {
   const { translations } = useContext(LocalizationContext);
   const [visibleModal, setVisibleModal] = useState(false);
+  const [isOpenVoting, setIsOpenVoting] = useState(false);
   const [votingObj, setVotingObj] = useState({ qr_code: null, url: null });
   const [errorType, setErrorType] = useState(null);
   const scorecardApi = new ScorecardApi();
@@ -37,44 +41,30 @@ const VotingQr = (props) => {
   useEffect(() => {
     const scorecard = Scorecard.find(props.route.params.scorecard_uuid);
     console.log('=== is scorecard opened = ', scorecard.is_open_voting);
+    setIsOpenVoting(scorecard.is_open_voting);
 
-    if (!scorecard.is_open_voting) {
-      scorecardProgressService.setOpenCloseVoting({
-        scorecardUuid: props.route.params.scorecard_uuid,
-        isOpen: true,
-        successCallback: () => {
-          Scorecard.update(props.route.params.scorecard_uuid, { is_open_voting: true });
-          downloadQrCode();
-        },
-        errroCallback: (error) => {
-          console.log('==== Error Open voting scorecard = ', error);
-        }
-      });
-    }
+    if (!scorecard.is_open_voting)
+      openVoting();
     else
       loadVotingQr();
   }, []);
 
-  // const openVoting = () => {
-  //   scorecardProgressService.setOpenCloseVoting({
-  //     scorecardUuid: props.route.params.scorecard_uuid,
-  //     isOpen: true,
-  //     successCallback: () => {
-  //       Scorecard.update(props.route.params.scorecard_uuid, { is_open_voting: true });
-
-  //       downloadQrCode();
-  //     },
-  //     errroCallback: (error) => {
-  //       console.log('==== Error Open voting scorecard = ', error);
-  //     }
-  //   });
-  // }
+  const openVoting = () => {
+    scorecardProgressService.setOpenCloseVoting({
+      scorecardUuid: props.route.params.scorecard_uuid,
+      isOpen: true,
+      successCallback: () => {
+        Scorecard.update(props.route.params.scorecard_uuid, { is_open_voting: true });
+        downloadQrCode();
+      },
+      errroCallback: (error) => {
+        console.log('==== Error Open voting scorecard = ', error);
+      }
+    });
+  }
 
   const loadVotingQr = () => {
     const scorecard = Scorecard.find(props.route.params.scorecard_uuid)
-    console.log('== qr code = ', scorecard.voting_qr);
-    // console.log('== voting url = ', scorecard.voting_url);
-
     setVotingObj({
       qr_code: scorecard.voting_qr,
       url: scorecard.voting_url
@@ -95,15 +85,6 @@ const VotingQr = (props) => {
     })
   }
 
-  const shareLink = () => {
-    Share.open({ url: votingObj.url, failOnCancel: false })
-      .catch((error) => {
-        // Todo: update the error message when failed to share the VOTING LINK
-        setErrorType(ERROR_SOMETHING_WENT_WRONG);
-        setVisibleModal(true)
-      })
-  }
-
   const _goNext = () => {
     const scorecard = Scorecard.find(props.route.params.scorecard_uuid);
     console.log('=== is scorecard open = ', scorecard.is_open_voting);
@@ -121,13 +102,38 @@ const VotingQr = (props) => {
     // });
   }
 
+  const openVotingButton = () => {
+    return (
+      <View style={{height: '100%', paddingTop: hp('2%')}}>
+        <EmptyListAction
+          title={translations.pleaseOpenVoting}
+          buttonLabel={translations.openVoting}
+          onPress={() => openVoting()}
+          customContainerStyle={{ marginTop: getDeviceStyle(wp('30%'), wp('45%')) }}
+        />
+      </View>
+    )
+  }
+
+  const downloadQrCodeButton = () => {
+    return (
+      <View style={{height: '100%', paddingTop: hp('2%')}}>
+        <EmptyListAction
+          title={translations.pleaseDownloadVotingQrCodeAndVotingUrl}
+          buttonLabel={translations.donwload}
+          onPress={() => downloadQrCode()}
+          customContainerStyle={{ marginTop: getDeviceStyle(wp('30%'), wp('45%')) }}
+        />
+      </View>
+    ) 
+  }
+
   const _renderBody = () => {
     const containerPaddingTop = scrollY.interpolate({
       inputRange: passProposeStepContainerPaddingTopInput,
       outputRange: passProposeStepContainerPaddingTopOutput,
       extrapolate: 'clamp',
     })
-    const screenWidth = Dimensions.get('screen').width;
 
     return (
       <React.Fragment>
@@ -143,45 +149,19 @@ const VotingQr = (props) => {
           >
 
             {/* Todo: show a download QR code button, if the scorecard is 'open_voting' and has not qr code yet */}
+            { (!isOpenVoting && !votingObj.qr_code) && openVotingButton() }
 
+            { (isOpenVoting && !votingObj.qr_code) && downloadQrCodeButton() }
 
-            {/* <TouchableOpacity onPress={() => downloadQrCode()}
-              style={{width: 100, height: 50, backgroundColor: 'green'}}
-            >
-              <Text>Download</Text>
-            </TouchableOpacity> */}
-
-            <Image source={{ uri: `file://${votingObj.qr_code}` }}
-              style={{width: screenWidth - 100, height: screenWidth - 100, alignSelf: 'center', marginTop: 22}}
-            />
-
-            <View style={{flexDirection: 'row', justifyContent: 'space-around', marginTop: 32, paddingHorizontal: 16}}>
-              <View style={{flex: 1}}>
-                <OutlinedButton
-                  icon="add-outline"
-                  label={translations.voting}
-                  onPress={() => {
-                    if (!!votingObj.url)
-                      Linking.openURL(votingObj.url);
-                  }}
-                  labelStyle={{textAlign: 'center', fontSize: getDeviceStyle(18, 14), marginTop: getDeviceStyle(6, 4), color: 'white'}}
-                  iconStyle={{color: 'white'}}
-                />
-              </View>
-
-              <View style={{width: 16}}/>
-
-              <View style={{flex: 1}}>
-                <OutlinedButton
-                  icon="share-outline"
-                  label={translations.shareLink}
-                  onPress={() => shareLink() }
-                  buttonStyle={{ backgroundColor: 'transparent', borderWidth: 2, borderColor: Color.primaryButtonColor }}
-                  labelStyle={{textAlign: 'center', fontSize: getDeviceStyle(18, 14), marginTop: getDeviceStyle(6, 4)}}
-                />
-              </View>
-            </View>
-            
+            { (isOpenVoting&& !!votingObj.qr_code) &&
+              <VotingQrCode
+                votingObj={votingObj}
+                showErrorModal={(type) => {
+                  setErrorType(type);
+                  setVisibleModal(true);
+                }}
+              />
+            }
 
           </ScrollView>
         </Animated.View>
